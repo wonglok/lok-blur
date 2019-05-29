874914bbe15a606bd2403fe84f1d32488ab7ad19
Wong Lok <yellowhappy831@gmail.com>
2019-05-29 21:00:05 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      OMG
+      test
     </div>
     <router-view/>
   </div>
